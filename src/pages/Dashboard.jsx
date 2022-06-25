7392c07412fb158d34cd9a5bc8dc9d1ff43bbee5
Ademola Adebowale <ademolajohn844@gmail.com>
2022-06-25 02:17:57 +0100

    Moralis authentication

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,49 +1,136 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Moralis from 'moralis';
+import { useEffect } from 'react';
+import { useMoralis, useMoralisWeb3Api } from 'react-moralis';
+import { ConnectButton, useNotification, NFTBalance } from 'web3uikit';
 import eth from '../assets/eth.png';
 import bnb from '../assets/bnb.png';
-import avalanche from '../assets/avalanche.png';
 import polygon from '../assets/polygon.png';
+import avalanche from '../assets/avalanche.png';
 import BalanceRow from '../components/BalanceRow';
 
-const Ethereum = 'Ethereum';
-const balance = '$200';
-const BSC = 'Bsc';
-const Avalanche = 'Avalanche';
+const Ethereum = 'Ethereum(Rinkeby)';
+const Ethereum2 = 'Ethereum(Ropsten)';
+const Ethereum3 = 'Ethereum(Kovan)';
+const Ethereum4 = 'Ethereum(Mainnet)';
+const staticData = '-----';
+const BSC = 'BSC';
 const Polygon = 'Polygon';
 
 const Dashboard = () => {
+  const dispatch = useNotification();
+  const Web3Api = useMoralisWeb3Api();
+  const { isAuthenticated, user } = useMoralis();
+
+  const userAddress = user?.get('ethAddress');
+
+  const [mainnetBalance, setMainnetBalance] = useState('0');
+  const [rinkebyBalance, setRinkebyBalance] = useState('0');
+  const [ropstenBalance, setRopstenBalance] = useState('0');
+  const [kovanBalance, setKovanBalance] = useState('0');
+
+  const handleNewNotification = ({ type, title, message, position }) => {
+    dispatch({
+      type: type || 'info',
+      message: message || '',
+      title: title || 'New Notification',
+      position: position || 'topR',
+    });
+  };
+
+  const fetchTokenBalances = async (chain) => {
+    const options = { chain, address: userAddress };
+    const result = await Web3Api.account.getNativeBalance(options);
+    console.log(result);
+    return result.balance;
+  };
+
+  const fetchBalances = async () => {
+    const balances = await Promise.all([
+      fetchTokenBalances('rinkeby'),
+      fetchTokenBalances('kovan'),
+      fetchTokenBalances('ropsten'),
+      fetchTokenBalances('mainnet'),
+    ]);
+
+    const rinkebyBalance = balances[0];
+    const kovanBalance = balances[1];
+    const ropstenBalance = balances[2];
+    const mainnetBalance = balances[3];
+
+    const mainnetBalanceEther = Moralis.Units.FromWei(mainnetBalance);
+    const rinkebyBalanceEther = Moralis.Units.FromWei(rinkebyBalance);
+    const kovanBalanceEther = Moralis.Units.FromWei(kovanBalance);
+    const ropstenBalanceEther = Moralis.Units.FromWei(ropstenBalance);
+
+    setMainnetBalance(mainnetBalanceEther);
+    setRinkebyBalance(rinkebyBalanceEther);
+    setRopstenBalance(ropstenBalanceEther);
+    setKovanBalance(kovanBalanceEther);
+  };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      // const notificationData = {
+      //   types: 'info',
+      //   title: 'Wallet Connected Successfully',
+      //   position: 'bottomR',
+      // };
+
+      // handleNewNotification(notificationData);
+
+      fetchBalances();
+    }
+  }, [isAuthenticated]);
+
   return (
     <div className="dashboard">
       <div className="container">
         <div className="ml-10 mt-20">
-          <h4 className="text-gray-300">Net Worth</h4>
+          {/* <h4 className="text-gray-300">Net Worth</h4>
           <h2 className="text-gray-300 font-bold text-3xl">$16,883.05</h2>
-          <h5 className="text-gray-300">Assets: $16,883.05</h5>
+          <h5 className="text-gray-300">Assets: $16,883.05</h5> */}
         </div>
 
         <div className="mt-10 mx-10">
           <h3 className="text-gray-300 font-bold">Networks</h3>
           &nbsp;
-          <div className="flex flex-wrap md:flex-nowrap">
-            <BalanceRow
-              logo={eth}
-              name={Ethereum}
-              balance={balance}
-              className="text-3xl"
-            />
-            <BalanceRow logo={bnb} name={BSC} balance={balance} />
-            <BalanceRow logo={polygon} name={Polygon} balance={balance} />
-            <br />
-            <BalanceRow logo={avalanche} name={Avalanche} balance={balance} />
-            <br />
-          </div>
-          <div>
-            <BalanceRow logo={avalanche} name={Avalanche} balance={balance} />
+          {isAuthenticated ? (
+            <div className="flex flex-wrap md:flex-nowrap">
+              <BalanceRow
+                logo={eth}
+                name={Ethereum}
+                balance={`${rinkebyBalance.slice(0, 10)} `}
+                className="text-3xl"
+              />
+              <BalanceRow
+                logo={eth}
+                name={Ethereum2}
+                balance={`${ropstenBalance.slice(0, 10)} `}
+              />
+              <BalanceRow
+                logo={eth}
+                name={Ethereum3}
+                balance={`${kovanBalance.slice(0, 10)} `}
+              />
+              <br />
+              {/* <BalanceRow logo={avalanche} name={Avalanche} balance={balance} /> */}
+              <br />
+            </div>
+          ) : (
+            <></>
+          )}
+          <div className="flex">
+            <BalanceRow logo={bnb} name={BSC} balance={staticData} />
+            <BalanceRow logo={polygon} name={Polygon} balance={staticData} />
           </div>
         </div>
 
         <div className="mt-10 mx-10">
           <h3 className="text-gray-300 font-bold">NFTs</h3>
+          <div className="w-full">
+            <NFTBalance address={userAddress} chain="rinkeby" />
+          </div>
         </div>
       </div>
     </div>
