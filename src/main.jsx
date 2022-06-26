b7d13d0712a7fc217f6b99a8e94baab5177f11ea
Ademola Adebowale <ademolajohn844@gmail.com>
2022-06-26 23:29:52 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { MoralisProvider } from 'react-moralis';
 import { NotificationProvider } from 'web3uikit';
-
+ 
 import './index.css';
 
 import { ContextProvider } from './contexts/ContextProvider';
-// import { CoinMarketProvider } from './contexts/CoinContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <MoralisProvider>
-      <NotificationProvider>
-        <ContextProvider>
-          <App />
-        </ContextProvider>
-      </NotificationProvider>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
     </MoralisProvider>
   </React.StrictMode>
 );
