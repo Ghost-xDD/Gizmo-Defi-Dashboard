f4edd8a326f8c3aeb15b7ba2e7a0b08dc5c751fe
Ademola Adebowale <ademolajohn844@gmail.com>
2022-07-01 17:14:27 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     >
       <ContextProvider>
         <CoinMarketProvider>
-          <App />
+          <App /> 
         </CoinMarketProvider>
       </ContextProvider>
     </MoralisProvider>
