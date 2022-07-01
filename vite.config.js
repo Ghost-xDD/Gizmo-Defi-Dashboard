e91fbcef956dc1c996e56338be2a1b5079dccde7
Ademola Adebowale <ademolajohn844@gmail.com>
2022-07-01 18:37:04 +0100

    update config

--- vite.config.js
+++ vite.config.js
@@ -2,31 +2,14 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
 
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
-  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
-
-  return defineConfig({
-    plugins: [react()],
-
-    server: {
-      port: process.env.VITE_SERVER_URL,
+  return {
+    define: {
+      _SERVER_URL: env._SERVER_URL,
+      _APP_ID: env._APP_ID,
     },
-    appId: process.env.VITE_APP_ID,
-  });
-};
-
-// export default defineConfig(({ command, mode }) => {
-//   const env = loadEnv(mode, process.cwd(), '');
-
-//   return {
-//     define: {
-//       _SERVER_URL: env._SERVER_URL,
-//       _APP_ID: env._APP_ID,
-//     },
-//     plugins: [react()],
-//   };
-// });
+    plugins: [react()],
+  };
+});
